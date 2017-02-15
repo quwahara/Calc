7ed88c28bdebb3bbc9c53c5be77cafc68100f5dc
mitsuaki kuwahara <quwahara@gmail.com>
2017-02-15 22:29:25 +0900

    Fix degrees

--- Calc/src/main/java/Parser.java
+++ Calc/src/main/java/Parser.java
@@ -15,10 +15,10 @@ public class Parser {
     public Parser() {
         degrees = new HashMap<>();
         degrees.put("(", 80);   // <-- Add
-        degrees.put("*", 70);
-        degrees.put("/", 70);
-        degrees.put("+", 60);
-        degrees.put("-", 60);
+        degrees.put("*", 60);
+        degrees.put("/", 60);
+        degrees.put("+", 50);
+        degrees.put("-", 50);
         degrees.put("=", 10);
         factorKinds = Arrays.asList(new String[] { "digit", "ident" }); // <-- Update
         binaryKinds = Arrays.asList(new String[] { "sign" });
