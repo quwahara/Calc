711f972008d628898a08f965875f44b15688ae23
mitsuaki kuwahara <quwahara@gmail.com>
2017-02-18 15:18:34 +0900

    Interpret single argument function

--- Calc/src/main/java/Interpreter.java
+++ Calc/src/main/java/Interpreter.java
@@ -33,6 +33,8 @@ public class Interpreter {
             return digit(expr);
         } else if (expr.kind.equals("ident")) {
             return ident(expr);
+        } else if (expr.kind.equals("func")) {
+            return func(expr);
         } else if (expr.kind.equals("paren")) {
             return invoke(expr);
         } else if (expr.kind.equals("sign") && expr.value.equals("=")) {
@@ -64,6 +66,30 @@ public class Interpreter {
             variables.put(name, v);
             return v;
         }
+    }
+
+    public Object func(Token token) throws Exception {
+        String name = token.ident.value;
+        if (functions.containsKey(name)) {
+            throw new Exception("Name was used");
+        }
+        if (variables.containsKey(name)) {
+            throw new Exception("Name was used");
+        }
+        String param = token.param.value;
+        if (functions.containsKey(param)) {
+            throw new Exception("Parameter name was used");
+        }
+        if (variables.containsKey(param)) {
+            throw new Exception("Parameter name was used");
+        }
+        DynamicFunc func = new DynamicFunc();
+        func.context = this;
+        func.name = name;
+        func.param = variable(ident(token.param));
+        func.block = token.block;
+        functions.put(name, func);
+        return null;
     }
 
     public Variable assign(Token expr) throws Exception {
@@ -146,7 +172,7 @@ public class Interpreter {
     public static abstract class Func {
         public String name;
 
-        abstract public Object invoke(Object arg);
+        abstract public Object invoke(Object arg) throws Exception;
     }
 
     public static class Println extends Func {
@@ -155,18 +181,37 @@ public class Interpreter {
         }
 
         @Override
-        public Object invoke(Object arg) {
+        public Object invoke(Object arg) throws Exception {
             System.out.println(arg);
             return null;
         }
     }
 
+    public static class DynamicFunc extends Func {
+
+        public Interpreter context;
+        public Variable param;
+        public List<Token> block;
+
+        @Override
+        public Object invoke(Object arg) throws Exception {
+            param.value = context.value(arg);
+            context.body(block);
+            return null;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        String text = "a = -1";
-        text += "println(a)";
+        String text = "";
+        text += "v = 0";
+        text += "function addV(num) {";
+        text += "  v = v + num";
+        text += "}";
+        text += "addV(3)";
+        text += "println(v)";
         List<Token> tokens = new Lexer().init(text).tokenize();
         List<Token> blk = new Parser().init(tokens).block();
         new Interpreter().init(blk).run();
-        // --> -1
+        // --> 3
     }
 }
