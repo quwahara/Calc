b3871f7222a82f7830d852a00b8a197380b7dde2
mitsuaki kuwahara <quwahara@gmail.com>
2017-03-25 16:42:47 +0900

    Import while parsing

--- Calc/src/main/java/Parser.java
+++ Calc/src/main/java/Parser.java
@@ -21,7 +21,7 @@ public class Parser {
         degrees.put("/", 60);
         degrees.put("+", 50);
         degrees.put("-", 50);
-        degrees.put("==", 40); // <-- Add
+        degrees.put("==", 40);
         degrees.put("!=", 40);
         degrees.put("<", 40);
         degrees.put("<=", 40);
@@ -33,8 +33,8 @@ public class Parser {
         factorKinds = Arrays.asList(new String[] { "digit", "ident" });
         binaryKinds = Arrays.asList(new String[] { "sign" });
         rightAssocs = Arrays.asList(new String[] { "=" });
-        unaryOperators = Arrays.asList(new String[] { "+", "-", "!" });                 // <-- Update
-        reserved = Arrays.asList(new String[] { "function", "return", "if", "else" });
+        unaryOperators = Arrays.asList(new String[] { "+", "-", "!" });
+        reserved = Arrays.asList(new String[] { "function", "return", "if", "else", "while"});  // <-- Update
     }
 
     private List<Token> tokens;
@@ -79,8 +79,13 @@ public class Parser {
                 token.left = expression(0);
             }
             return token;
-        } else if (token.kind.equals("ident") && token.value.equals("if")) {    // <-- Add
+        } else if (token.kind.equals("ident") && token.value.equals("if")) {
             return if_(token);
+        } else if (token.kind.equals("ident") && token.value.equals("while")) { // <-- Add
+            return while_(token);
+        } else if (token.kind.equals("ident") && token.value.equals("break")) { // <-- Add
+            token.kind = "brk";
+            return token;
         } else if (factorKinds.contains(token.kind)) {
             return token;
         } else if (unaryOperators.contains(token.value)) {
@@ -136,6 +141,20 @@ public class Parser {
         return token;
     }
 
+    private Token while_(Token token) throws Exception {
+        token.kind = "while";
+        consume("(");
+        token.left = expression(0);
+        consume(")");
+        if (token().value.equals("{")) {
+            token.block = body();
+        } else {
+            token.block = new ArrayList<Token>();
+            token.block.add(expression(0));
+        }
+        return token;
+    }
+    
     private Token ident() throws Exception {
         Token id = next();
         if (!id.kind.equals("ident")) {
--- Calc/src/test/java/ParsrTest.java
+++ Calc/src/test/java/ParsrTest.java
@@ -589,4 +589,76 @@ public class ParsrTest {
                 block.get(0).indent(""));
     }
 
+    @Test
+    public void while1() {
+        text = "while";
+        try {
+            block = parser.init(lexer.init(text).tokenize()).block();
+            fail();
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void while2() {
+        text = "while(";
+        try {
+            block = parser.init(lexer.init(text).tokenize()).block();
+            fail();
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void while3() {
+        text = "while(1";
+        try {
+            block = parser.init(lexer.init(text).tokenize()).block();
+            fail();
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void while4() {
+        text = "while(1)";
+        try {
+            block = parser.init(lexer.init(text).tokenize()).block();
+            fail();
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void while5() {
+        text = "while(1){";
+        try {
+            block = parser.init(lexer.init(text).tokenize()).block();
+            fail();
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void while6() throws Exception {
+        text = "while(1){}";
+        block = parser.init(lexer.init(text).tokenize()).block();
+        assertEquals(1, block.size());
+        assertEquals("" + "while \"while\"\n" + "[left]\n" + "  digit \"1\"\n" + "[block]\n" + "",
+                block.get(0).indent(""));
+    }
+
+    @Test
+    public void while7() throws Exception {
+        text = "while(1) 3";
+        block = parser.init(lexer.init(text).tokenize()).block();
+        assertEquals(1, block.size());
+        assertEquals("" + "while \"while\"\n" + "[left]\n" + "  digit \"1\"\n" + "[block]\n" + "  digit \"3\"\n" + "",
+                block.get(0).indent(""));
+    }
 }
